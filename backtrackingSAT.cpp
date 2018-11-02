9e51afde8ea374e2f89eca763502bd6d32654f59
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-11-01 21:26:52 -0700

    didn't do much, but started coding

--- backtrackingSAT.cpp
+++ backtrackingSAT.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+int MAX = 1024;
 int nbvar = 0; // total number of variables in input file
 int nbclauses = 0; // total number of clauses in input file
 
@@ -20,10 +21,10 @@ int str_to_int (string str){
 }
 
 // parses input file into a vector of clauses (vecotrs of string values)
-vector<vector<string> > inputReadFunc(vector<vector<string> > &clause_vector){
+vector<vector<int> > inputReadFunc(vector<vector<int> > &clause_vector){
 	string word;
 	ifstream inFile;
-	vector<string> word_vector;
+	vector<int> var_vector;
 	int input_index = 0;
 
     inFile.open("DIMACSfile.txt");
@@ -52,20 +53,30 @@ vector<vector<string> > inputReadFunc(vector<vector<string> > &clause_vector){
 		}
 		if (word == "0"){
 			input_index++;
-			clause_vector.push_back(word_vector);
-			word_vector.clear();
+			clause_vector.push_back(var_vector);
+			var_vector.clear();
 			continue;
 		}
 		input_index++;
-		word_vector.push_back(word);
+		var_vector.push_back(str_to_int(word));
 	}
 
 	inFile.close();
 	return clause_vector;
 }
 
+// implements backtracking search
+vector<int> backtrack(vector<int> var_vector){
+	if (var_vector.size() == nbvar){
+		return var_vector;
+	}
+
+	
+	return var_vector;
+}
+
 int main(){
-	vector<vector<string> > clause_vector; // vector of clauses
+	vector<vector<int> > clause_vector; // vector of clauses
 	clause_vector = inputReadFunc(clause_vector);
 
 	//output test
@@ -78,6 +89,20 @@ int main(){
 	cout << endl;
 
 	cout << nbvar << endl << nbclauses << endl;
+
+	//treat like a constraint satisfaction problem - solution is a list of assigned boolean values, constraint
+	//			is the list of clauses
+		// v x1 x2 x3 (...)
+	// build binary # of size nbvar
+	// take a guess
+	//		is problem solved?
+	// 			yes -> done
+	//			no -> continue
+	//			contradiction -> backtrack
+	vector<int> var_vector; // initialize vector of variables
+	var_vector.push_back(0); // give initial guess
+	vector<int> solution_vector = backtrack(var_vector);
+
 
 	return 0;
 }
