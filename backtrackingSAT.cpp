e11f621846a66f43632bc2817d4ab0ac589ee834
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-11-02 18:00:44 -0700

    some code... still need to write check clauses

--- backtrackingSAT.cpp
+++ backtrackingSAT.cpp
@@ -11,6 +11,8 @@ using namespace std;
 int MAX = 1024;
 int nbvar = 0; // total number of variables in input file
 int nbclauses = 0; // total number of clauses in input file
+int current_var_depth = 0;
+vector<vector<int> > clause_vector; // vector of clauses
 
 // converts strings to ints
 int str_to_int (string str){
@@ -65,21 +67,47 @@ vector<vector<int> > inputReadFunc(vector<vector<int> > &clause_vector){
 	return clause_vector;
 }
 
+//checks sign of a value
+int sign(int x){
+	if (x>0)
+		return 1;
+	if (x<0)
+		return -1;
+	return 0;
+}
+
+// checks whether clauses are satisfied
+bool clause_check(int val, int current_var_depth){
+	return true;
+}
+
 // implements backtracking search
 vector<int> backtrack(vector<int> var_vector){
 	if (var_vector.size() == nbvar){
 		return var_vector;
 	}
-
-	
+	current_var_depth++;
+	for (int i = 1; i>0; i--){
+		if (clause_check(i, current_var_depth)){
+			var_vector.push_back(i);
+			vector<int> res;
+			res = backtrack(var_vector);
+			if (!res.empty()){
+				return res;
+			}
+			var_vector.pop_back();
+		}
+	}
+	cout << "backtracking failed" << endl;
+	var_vector.clear();
+	var_vector.push_back(0);
 	return var_vector;
 }
 
 int main(){
-	vector<vector<int> > clause_vector; // vector of clauses
 	clause_vector = inputReadFunc(clause_vector);
 
-	//output test
+	//output problem
 	for (int i = 0; i<clause_vector.size(); i++){
 		for (int j = 0; j<clause_vector[i].size(); j++){
 			cout << clause_vector[i][j] << " ";
@@ -88,7 +116,7 @@ int main(){
 	}
 	cout << endl;
 
-	cout << nbvar << endl << nbclauses << endl;
+	cout << "num vars: " << nbvar << endl << "num clauses: " << nbclauses << endl;
 
 	//treat like a constraint satisfaction problem - solution is a list of assigned boolean values, constraint
 	//			is the list of clauses
@@ -99,10 +127,18 @@ int main(){
 	// 			yes -> done
 	//			no -> continue
 	//			contradiction -> backtrack
+
 	vector<int> var_vector; // initialize vector of variables
 	var_vector.push_back(0); // give initial guess
 	vector<int> solution_vector = backtrack(var_vector);
 
+	// output solution
+	cout << "v ";
+	for (int i = 0; i<solution_vector.size(); i++){
+		cout << solution_vector[i] << " ";
+	}
+	cout << endl;
+	
 
 	return 0;
 }
