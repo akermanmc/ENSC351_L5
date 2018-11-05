762c1e3a81755848c35d671d1dcabe6acb5b9837
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-11-04 16:29:12 -0800

    Needs log and threading added

--- backtrackingSAT.cpp
+++ backtrackingSAT.cpp
@@ -5,14 +5,19 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <omp.h>
 
 using namespace std;
 
 int MAX = 1024;
 int nbvar = 0; // total number of variables in input file
 int nbclauses = 0; // total number of clauses in input file
-int current_var_depth = 0;
+int var_depth = 0; // depth of variable backtracking
+
 vector<vector<int> > clause_vector; // vector of clauses
+vector<int> var_vector; 			// solution vector
+vector<int> temp_vector;			// temp vector of variables
 
 // converts strings to ints
 int str_to_int (string str){
@@ -76,8 +81,54 @@ int sign(int x){
 	return 0;
 }
 
-// checks whether clauses are satisfied
-bool clause_check(int val, int current_var_depth){
+// checks whether a particular clause is satisfied
+bool clause_check(vector<int> clause, int val){
+	int num_vars_found = 0; // keeps track of whether the given variable (neg or pos) is in the clause
+	bool clause_satisfied = false;
+	
+	for (int i = 0; i<(var_depth); i++){
+		// if variable is not found in clause at all return true
+		if((find(clause.begin(), clause.end(), (i+1)) == clause.end()) && (find(clause.begin(), clause.end(), -(i+1)) == clause.end())){
+			continue;
+		}
+		// if variable is neg and in clause return true
+		if(find(clause.begin(), clause.end(), -(i+1)) != clause.end()){
+			num_vars_found++;
+			if (temp_vector[i] == 0){
+				clause_satisfied = true;
+				continue;
+			}
+		}
+		// if variable  is pos and in clause return true
+		if (find(clause.begin(), clause.end(), (i+1)) != clause.end()){
+			num_vars_found++;
+			if (temp_vector[i] == 1){
+				clause_satisfied = true;
+				continue;
+			}
+		}
+	}
+	// case when clause is not satisfied:
+	//cout << num_vars_found << clause.size() << var_depth << endl;
+	if ((num_vars_found == clause.size()) && (clause_satisfied == false)){
+		return false;
+	}
+	return true;
+}
+
+// checks whether all clauses are satisfied
+bool check_all_clauses(int val){
+
+	//#pragma omp parallel for
+	for(int i = 0; i < clause_vector.size(); i++){
+		if (clause_check(clause_vector[i], val)){
+			continue;
+		}else{
+			//cout << "check_all_clauses returning false" << endl;
+			return false;
+		}
+	}
+	//cout << "check_all_clauses returning true" << endl;
 	return true;
 }
 
@@ -86,9 +137,13 @@ vector<int> backtrack(vector<int> var_vector){
 	if (var_vector.size() == nbvar){
 		return var_vector;
 	}
-	current_var_depth++;
-	for (int i = 1; i>0; i--){
-		if (clause_check(i, current_var_depth)){
+	// current variable is equal to var_depth
+	var_depth++;
+
+	// for loop determines variable's value
+	for (int i = 1; i>(-1); i--){
+		temp_vector.push_back(i);
+		if (check_all_clauses(i)){
 			var_vector.push_back(i);
 			vector<int> res;
 			res = backtrack(var_vector);
@@ -97,17 +152,18 @@ vector<int> backtrack(vector<int> var_vector){
 			}
 			var_vector.pop_back();
 		}
+		temp_vector.pop_back();
 	}
-	cout << "backtracking failed" << endl;
-	var_vector.clear();
-	var_vector.push_back(0);
-	return var_vector;
+	var_depth--;
+	//cout << "backtracking" << endl;
+	vector<int> empty_vector;
+	return empty_vector;
 }
 
 int main(){
 	clause_vector = inputReadFunc(clause_vector);
 
-	//output problem
+	//output problem formulation
 	for (int i = 0; i<clause_vector.size(); i++){
 		for (int j = 0; j<clause_vector[i].size(); j++){
 			cout << clause_vector[i][j] << " ";
@@ -115,30 +171,29 @@ int main(){
 		cout << endl;
 	}
 	cout << endl;
-
 	cout << "num vars: " << nbvar << endl << "num clauses: " << nbclauses << endl;
 
-	//treat like a constraint satisfaction problem - solution is a list of assigned boolean values, constraint
-	//			is the list of clauses
-		// v x1 x2 x3 (...)
-	// build binary # of size nbvar
-	// take a guess
-	//		is problem solved?
-	// 			yes -> done
-	//			no -> continue
-	//			contradiction -> backtrack
-
-	vector<int> var_vector; // initialize vector of variables
-	var_vector.push_back(0); // give initial guess
+	// call backtracking function
 	vector<int> solution_vector = backtrack(var_vector);
 
+	// TO DO: 
+// need to "print a progress report on how many backtracks have occurred to date (cumulative) every two seconds"
+
 	// output solution
+	cout << endl << "Solution: " << endl;
+	// for (int i = 0; i<solution_vector.size(); i++){
+	// 	cout << solution_vector[i] << " ";
+	// }
+	// cout << endl;
 	cout << "v ";
 	for (int i = 0; i<solution_vector.size(); i++){
-		cout << solution_vector[i] << " ";
+		if (solution_vector[i] == 0)
+			cout << -(i+1);
+		if (solution_vector[i] == 1)
+			cout << (i+1);
+		cout << " ";
 	}
 	cout << endl;
-	
 
 	return 0;
 }
